e163b93bfcfbb511101e8e45e69d875a5a154ce6
Bit-mini <bit@pix-bit.com>
2013-02-08 01:21:30 -0500

    New from scratch List Detail App for iNEPA BOR

--- BlogReader/src/com/pixbit/blogreader/MainListActivity.java
+++ BlogReader/src/com/pixbit/blogreader/MainListActivity.java
@@ -1,15 +1,29 @@
 package com.pixbit.blogreader;
 
 import android.app.ListActivity;
+import android.content.res.Resources;
 import android.os.Bundle;
 import android.view.Menu;
+import android.widget.ArrayAdapter;
 
 public class MainListActivity extends ListActivity {
+	
+	protected String[] mAndroidNames;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_list);
+        
+        Resources resources = getResources();
+        mAndroidNames = resources.getStringArray(R.array.android_names);
+        
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, mAndroidNames);
+        setListAdapter(adapter);
+        
+        //Toast.makeText(this, getString(R.string.no_items), Toast.LENGTH_LONG).show();
+        
+        
     }
 
     @Override
