9b45d65c5af41e1781e5362567db97ab18ab2b48
Bit-mini <bit@pix-bit.com>
2013-02-21 17:46:04 -0500

    Got the home button to work and some other ACTIONBAR methods

---
+++ iNEPABOR/assets/js/myScript.js
@@ -0,0 +1,115 @@
+// global array to store scroll values
+var scrollValues = new Array();
+
+// We're using a global variable to store the number of occurrences
+var MyApp_SearchResultCount = 0;
+
+// helper function, recursively searches in elements and their child nodes
+function MyApp_HighlightAllOccurencesOfStringForElement(element,keyword) {
+	if (element) {
+		if (element.nodeType == 3) {        // 3 is a Text node
+			while (true) {
+				// If the object is a TextNode, the nodeValue property returns a string representing the text contained by the node.
+				var value = element.nodeValue;  // Search for keyword in text node and returns the paragraph its in
+				var idx = value.toLowerCase().indexOf(keyword);
+				
+				if (idx < 0) break;             // not found, abort
+				
+				var span = document.createElement("span");
+				var text = document.createTextNode(value.substr(idx,keyword.length));
+				span.appendChild(text);
+				span.setAttribute("class","MyAppHighlight");
+				span.style.backgroundColor="yellow";
+				span.style.color="black";
+				text = document.createTextNode(value.substr(idx+keyword.length));
+				element.deleteData(idx, value.length - idx);
+				var next = element.nextSibling;
+				element.parentNode.insertBefore(span, next);
+				element.parentNode.insertBefore(text, next);
+				element = text;
+				MyApp_SearchResultCount++;	// update the counter
+				//scrollArray[MyApp_SearchResultCount-1] = scrollValue
+				//alert(MyApp_SearchResultCount-1);
+			}
+		} else if (element.nodeType == 1) { // Element node
+			if (element.style.display != "none" && element.nodeName.toLowerCase() != 'select') {
+				for (var i=element.childNodes.length-1; i>=0; i--) {
+					MyApp_HighlightAllOccurencesOfStringForElement(element.childNodes[i],keyword);
+				}
+			}
+		}
+	}
+}
+
+function getElementsByClassName(classname, node)  {
+	if(!node) node = document.getElementsByTagName("body")[0];
+	var a = [];
+	var re = new RegExp('\\b' + classname + '\\b');
+	var els = node.getElementsByTagName("*");
+	for(var i=0,j=els.length; i<j; i++)
+		if(re.test(els[i].className))a.push(els[i]);
+	return a;
+}
+
+// the main entry point to start the search
+function MyApp_HighlightAllOccurencesOfString(keyword) {
+	MyApp_RemoveAllHighlights();
+	MyApp_HighlightAllOccurencesOfStringForElement(document.body, keyword.toLowerCase());
+	$.scrollTo( 0, 800 );
+}
+
+// helper function, recursively removes the highlights in elements and their childs
+function MyApp_RemoveAllHighlightsForElement(element) {
+	if (element) {
+		if (element.nodeType == 1) {
+			if (element.getAttribute("class") == "MyAppHighlight") {
+				var text = element.removeChild(element.firstChild);
+				element.parentNode.insertBefore(text,element);
+				element.parentNode.removeChild(element);
+				return true;
+			} else {
+				var normalize = false;
+				for (var i=element.childNodes.length-1; i>=0; i--) {
+					if (MyApp_RemoveAllHighlightsForElement(element.childNodes[i])) {
+						normalize = true;
+					}
+				}
+				if (normalize) {
+					element.normalize();
+				}
+			}
+		}
+	}
+	return false;
+}
+
+// the main entry point to remove the highlights
+function MyApp_RemoveAllHighlights() {
+	MyApp_SearchResultCount = 0;
+	MyApp_RemoveAllHighlightsForElement(document.body);
+}
+
+function getSearchScrollValues() {
+	var x = getElementsByClassName("MyAppHighlight", document.body);
+	for (var i=0; i<=x.length-1; i++) {
+		scrollValues[i]=x[i].offsetTop;
+	}
+	var result = "";
+	for (var i=0; i<=x.length-1; i++) {
+		result += scrollValues[i] + " ";
+	}
+	//alert(result);
+	return result;
+}
+
+function portraitZoom()
+{
+	alert("portrait");
+	//document.getElementById("viewport").setAttribute('content','width = 320, initial-scale = 2.0, minimum-scale = 1.0, maximum-scale = 10.0');
+}
+
+function landscapeZoom()
+{
+	alert("landscape");
+	//document.getElementById("viewport").setAttribute('content','width = 320, initial-scale = 2.0, minimum-scale = 1.0, maximum-scale = 10.0');
+}
--- iNEPABOR/src/com/empsi/inepabor/MainActivity.java
+++ iNEPABOR/src/com/empsi/inepabor/MainActivity.java
@@ -1,66 +1,40 @@
 package com.empsi.inepabor;
 
-import java.io.InputStream;
-import java.util.List;
-
-import javax.xml.parsers.SAXParser;
-import javax.xml.parsers.SAXParserFactory;
-
-import org.xml.sax.InputSource;
-import org.xml.sax.XMLReader;
-
-import android.app.Activity;
 import android.content.Intent;
-import android.content.res.Resources;
 import android.os.Bundle;
-import android.util.Log;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 import android.widget.Toast;
 
-public class MainActivity extends Activity {
+import com.actionbarsherlock.app.ActionBar;
+import com.actionbarsherlock.app.SherlockActivity;
+import com.actionbarsherlock.view.MenuItem;
 
-	public static final String TAG = MainActivity.class.getSimpleName();
-	public List<ParsedRow> entries;
-	public List<ParsedRow> currentEntries;
-	public List<ParsedRow> previousEntries;
-	public List<String> titleList;
+public class MainActivity extends SherlockActivity {
 
+	public class JavaScriptHandler {
+	    MainActivity parentActivity;
+	    public JavaScriptHandler(MainActivity activity) {
+	        parentActivity = activity;
+	    }
+	 
+	    public void setResult(int val){
+//	        this.parentActivity.javascriptCallFinished(val);
+	    }
+	 
+	    public void calcSomething(int x, int y){
+//	        this.parentActivity.changeText("Result is : " + (x * y));
+	    }
+	}
+	
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        
-        try {
-            /* Get a SAXParser from the SAXPArserFactory. */
-            SAXParserFactory spf = SAXParserFactory.newInstance();
-            SAXParser sp = spf.newSAXParser();
 
-            /* Get the XMLReader of the SAXParser we created. */
-            XMLReader xr = sp.getXMLReader();
-            /* Create a new ContentHandler and apply it to the XML-Reader*/
-            PlistHandler myHandler = new PlistHandler();
-            xr.setContentHandler(myHandler);
-            
-            /* Parse the xml-data from our file. */
-            Resources res = getResources();
-            InputStream is = res.openRawResource(R.raw.data);
-            xr.parse(new InputSource(is));
-            /* Parsing has finished. */
-
-            /* Our PlistHandler now provides the parsed data to us. */
-        		this.entries = myHandler.getListOfRows();
-//        		Log.d(TAG, "Number of entries on this list: " + Integer.toString(entries.size()));
-        		
-        		this.currentEntries = this.entries;
-            
-            /* Set the result to be displayed in our GUI. */
-//          Toast.makeText(this, parsedExampleDataSet.toString(), Toast.LENGTH_SHORT).show();
-           
-     	} catch (Exception e) {
-        		/* Display any Error to the GUI. */
-        		Log.e(TAG, "XMLQueryError", e);
-        	}
+        getSupportActionBar().setIcon(R.drawable.mr_bit);
+        final ActionBar actionBar = getSupportActionBar();
+        actionBar.setDisplayHomeAsUpEnabled(true);
         
         Intent receiveIntent = getIntent();
 //        String content = receiveIntent.getData().toString();
@@ -79,18 +53,9 @@ public class MainActivity extends Activity {
             // Do something with the data
         		WebView webview = (WebView) findViewById(R.id.web_engine);
         		webview.getSettings().setJavaScriptEnabled(true);
-        		webview.setWebViewClient(new WebViewClient() {
-        		    @Override
-        		    public void onPageFinished(WebView view, String url)
-        		    {
-        		        view.loadUrl("javascript:(function() { " +
-        		                "document.getElementsByTagName('body')[0].style.color = 'red'; " +
-        		                "})()");
-        		    }
-        		});
-        		
-        		
+        		webview.setWebViewClient(new WebViewClient());
             webview.loadUrl(view);
+            webview.loadUrl("javascript:jQuery('html, body').scrollTop(jQuery('#bookmark60').offset().top);");
         }
         if (title != null) {
             // Do something with the data
@@ -102,7 +67,22 @@ public class MainActivity extends Activity {
         }
         
     }
-//
+
+  @Override
+  public boolean onMenuItemSelected(int featureId, MenuItem item) {
+
+	    int itemId = item.getItemId();
+	    switch (itemId) {
+	    case android.R.id.home:
+//	        toggle();
+	    		finish();
+	        Toast.makeText(this, "home pressed", Toast.LENGTH_LONG).show();
+	        break;
+	    }
+
+	    return true;
+	}
+
 //    @Override
 //    public boolean onCreateOptionsMenu(Menu menu) {
 //        // Inflate the menu; this adds items to the action bar if it is present.
